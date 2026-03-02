3542183c62eb7e13829834c25b6e0700ba5941a5
Larry Mitchell <118214035+lmitchell137@users.noreply.github.com>
2026-03-02 15:57:18 -0500

    chore: add module-info.java for dynamisai-extensions

---
+++ dynamisai-extensions/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.dynamisai.extensions {
+    requires dynamis.core;
+    requires org.dynamisai.core;
+    requires org.dynamisai.cognition;
+    requires org.dynamisai.perception;
+    requires org.dynamisai.memory;
+    requires org.dynamisai.navigation;
+    requires org.dynamisai.voice;
+
+    exports org.dynamisai.extensions;
+}
